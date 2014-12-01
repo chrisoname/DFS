3e9e76252633bf847aa9bbc1d90a3ae94e76727a
eramos94 <edwinomar1994@gmail.com>
2014-12-01 02:20:44 -0400

    TERMINAMOS.yay

--- cptest.sh
+++ cptest.sh
@@ -1,4 +1,4 @@
 cp meta-data.py inception.txt
 clear
-python copy.py Packet.py localhost:8000:/home/file.txt
-python copy.py localhost:8000:/home/file.txt TERMINAMOSPU~ETA.yay
+python copy.py inception.txt localhost:8000:/home/file1.txt
+python copy.py localhost:8000:/home/file1.txt TERMINAMOSPU~ETA.yay
